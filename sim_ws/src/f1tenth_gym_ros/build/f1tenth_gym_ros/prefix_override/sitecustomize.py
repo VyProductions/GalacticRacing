c39fe24c0e3d9b9bebcde2a139ff211d227c3922
VyProductions <VyProductions.music@gmail.com>
2024-01-31 15:55:58 -0800

    Trying again to use VCS with this simulator

---
+++ sim_ws/src/f1tenth_gym_ros/build/f1tenth_gym_ros/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/vy/GalacticRacing/sim_ws/src/f1tenth_gym_ros/install/f1tenth_gym_ros'
---
+++ sim_ws/src/ros_msg_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'ros_msg_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker_node = ros_msg_pkg.talker_node:main',
+            'relay_node = ros_msg_pkg.relay_node:main'
+        ],
+    },
+)
