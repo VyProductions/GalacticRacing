5aa6a74bc01982af24170fd023afd542e823fc4a
VyProductions <VyProductions.music@gmail.com>
2024-02-19 22:04:45 -0800

    also wtf

--- sim_ws/src/install/gap_follow/lib/gap_follow/reactive_node.py
+++ sim_ws/src/install/gap_follow/lib/gap_follow/reactive_node.py
@@ -7,6 +7,8 @@ import numpy as np
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
+import copy
+
 from typing import (
     List, Tuple, Dict
 )
@@ -29,12 +31,11 @@ WIND_SIZE  : int = 5
 FOV : float = deg_to_rad(270.0)
 
 # distance processing
-DIST_THRESH  : float32 = 3.00 # meters
-DISP_EPSILON : float32 = 0.60 # meters
-CORN_EPSILON : float32 = 0.02 # meters
+DIST_THRESH  : float32 = 6.00 # meters
+DISP_EPSILON : float32 = 0.20 # meters
 
 # car dimensions
-CAR_WIDTH  : float32 = 0.45 # meters
+CAR_WIDTH : float32 = 0.40 # meters
 
 def idx_to_rad(idx : int) -> float32:
     return idx * FOV / (RANGE_SIZE - 1) - FOV / 2.0
@@ -69,10 +70,6 @@ class ReactiveFollowGap(Node):
         self.laser_pub = self.create_publisher(
             LaserScan, scan_mod_topic, SCAN_REFRESH
         )
-
-        self.steering_angle = 0.0 # rads relative to forward (left: > 0.0, right: < 0.0)
-
-        self.kp = 1.0
     
     def preprocess_lidar(self, ranges):
         """ Preprocess the LiDAR scan array. Expert implementation includes:
@@ -82,81 +79,113 @@ class ReactiveFollowGap(Node):
 
         for i in range(RIGHT, LEFT + 1):
             if ranges[i] > DIST_THRESH:
-                ranges[i] = DIST_THRESH # too far away; make it "infinity"
+                ranges[i] = float("inf")
+                # ranges[i] = DIST_THRESH
             elif ranges[i] == float("nan"):
                 ranges[i] = 0.0
-
+    
     def disparity_extender(self, ranges):
-        disp_ranges = list(ranges)
+        disp_ranges = copy.deepcopy(ranges) # deep copy of ranges
+        disp_idxs : List[Tuple[int, float32, float32]] = [] # list of disparity indices, distances, and differences
+        disp_list : List[Tuple[int, str]] = []
+        radius : float32 = CAR_WIDTH / 2
 
-        for i in range(RIGHT, LEFT):
-            idx      : int = i      # query
-            next_idx : int = i + 1  # left of query
+        for i in range(RIGHT + 180, LEFT - 180):
+            right_idx : int = i
+            left_idx  : int = i + 1
 
-            left_diff : float32 = ranges[idx] - ranges[next_idx]
+            diff : float32 = ranges[left_idx] - ranges[right_idx]
+            theta : float32 = 0.0 # rad
 
-            if abs(left_diff) > DISP_EPSILON:
-                if left_diff < 0:  # object leaving scope
-                    theta : float32 = 0.0
+            if abs(diff) > DISP_EPSILON:
+                if diff < 0:  # left side is closer than right side
+                    dist : float32 = ranges[left_idx]
 
-                    if abs(CAR_WIDTH / (2 * ranges[i])) < 1:
-                        theta : float32 = np.arcsin(CAR_WIDTH / (2 * ranges[i]))
+                    if abs(radius / dist) < np.pi/2:
+                        theta = np.arctan(radius / dist)
+                    
+                    targ_idx  : int = rad_to_idx(idx_to_rad(left_idx) - theta)
+                    targ_offs : int = left_idx - targ_idx
 
-                    targ_idx : int = rad_to_idx(idx_to_rad(i) + theta)
-                    targ_offs = targ_idx - i
-
-                    for j in range(targ_offs):
-                        if ranges[i + j] > ranges[i]:
-                            disp_ranges[i + j] = ranges[i]
-                else:                  # object entering scope
-                    theta : float32 = 0.0
-
-                    if abs(CAR_WIDTH / (2 * ranges[i + 1])) < 1:
-                        theta : float32 = np.arcsin(CAR_WIDTH / (2 * ranges[i + 1]))
-
-                    targ_idx : int = rad_to_idx(idx_to_rad(i + 1) - theta)
-                    targ_offs = i + 1 - targ_idx
+                    disp_idxs.append((targ_idx, dist, diff))
 
                     for j in range(targ_offs):
-                        if disp_ranges[i - j] > disp_ranges[i + 1]:
-                            disp_ranges[i - j] = ranges[i + 1]
+                        if ranges[left_idx] < disp_ranges[right_idx - j]:
+                            disp_ranges[right_idx - j] = ranges[left_idx]
+                else:         # right side is closer than left side
+                    dist : float32 = ranges[right_idx]
 
-        return disp_ranges
+                    if abs(radius / dist) < np.pi/2:
+                        theta = np.arctan(radius / dist)
+                    
+                    targ_idx  : int = rad_to_idx(idx_to_rad(right_idx) + theta)
+                    targ_offs : int = targ_idx - right_idx
 
+                    disp_idxs.append((targ_idx, dist, diff))
+
+                    for j in range(targ_offs):
+                        if ranges[right_idx] < disp_ranges[left_idx + j]:
+                            disp_ranges[left_idx + j] = ranges[right_idx]
+        
+        for index, dist, range_diff in disp_idxs:
+            if disp_ranges[index] == dist:
+                disp_list.append((index, "Leading" if range_diff < 0 else "Trailing"))
+
+        return disp_ranges, disp_list
+    
     def furthest_idx(self, ranges):
-        max_right : int = RIGHT
-        max_left  : int = LEFT
+        Rmax_idx : int = RIGHT
+        Lmax_idx : int = LEFT
 
-        for i in range(RIGHT + 1, LEFT + 1):
-            if ranges[i] > ranges[max_right]:
-                max_right = i
+        for i in range(RIGHT + 1, LEFT):
+            if ranges[i] > ranges[Rmax_idx]:
+                Rmax_idx = i
         
         for i in range(LEFT, RIGHT - 1, -1):
-            if ranges[i] > ranges[max_left]:
-                max_left = i
-        
-        return (max_left + max_right) // 2
+            if ranges[i] > ranges[Lmax_idx]:
+                Lmax_idx = i
+
+        print(f"Left: {Lmax_idx}, Right: {Rmax_idx}")
+        return (Rmax_idx + Lmax_idx) // 2
+    
+    def bubble(self, ranges):
+        close_idx : int = self.closest_idx(ranges)
+        theta : float32 = 1.1 * np.arcsin(CAR_WIDTH / (2 * ranges[close_idx]))
+        right_idx : int = rad_to_idx(idx_to_rad(close_idx) - theta)
+        left_idx  : int = rad_to_idx(idx_to_rad(close_idx) + theta)
+
+        if ranges[close_idx] < 1.5:
+            for i in range(right_idx, left_idx + 1):
+                ranges[i] = 0.0
+
+    def closest_idx(self, ranges):
+        min_idx : int = 540
+
+        for i in range(RIGHT + 235, LEFT - 234):
+            if ranges[i] < ranges[min_idx]:
+                min_idx = i
+
+        return min_idx
 
     def lidar_callback(self, data):
         """
         Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
         """
-        ranges = list(data.ranges)
-        new_scan : LaserScan = data
-        
-        # preprocess range data
+
+        # deep copy of laser scan message
+        new_scan : LaserScan = copy.deepcopy(data)
+
+        # preprocess scan ranges
         self.preprocess_lidar(new_scan.ranges)
 
-        # filter range data using disparity extender algorithm
-        new_scan.ranges = self.disparity_extender(new_scan.ranges)
+        disp_ranges, disp_list = self.disparity_extender(new_scan.ranges)
+        self.bubble(disp_ranges)
 
-        # get best index by finding the largest gap and aiming towards its center
-        # best_idx = self.max_gap(new_scan.ranges)
+        new_scan.ranges = disp_ranges
 
         self.laser_pub.publish(new_scan)
 
-        # get best index by finding longest distance in range and aiming there
-        best_idx = self.furthest_idx(new_scan.ranges)
+        best_idx : int = self.furthest_idx(disp_ranges)
 
         # Determine steering angle from best point, if valid
         angle : float32
@@ -172,22 +201,23 @@ class ReactiveFollowGap(Node):
         elif angle > deg_to_rad(20.0):
             angle = deg_to_rad(20.0)
 
-        # print(f"Steering to angle: {rad_to_deg(angle)}")
-        self.steering_angle = angle
-        # self.steering_angle = 0.0
-
         # Adjust velocity based on distance
         velocity : float32
-        if new_scan.ranges[540] < 0.75:
-            velocity = 0.0
+        if data.ranges[540] < 0.75:
+            velocity = -1.0
+            angle    = -angle
+        elif data.ranges[540] > DIST_THRESH:
+            velocity = 2.0
+        elif data.ranges[540] < 2.0:
+            velocity = 1.0
         else:
-            velocity = ranges[540] * 0.8
+            velocity = 1.0
 
         #Publish Drive message
         msg = AckermannDriveStamped()
-        # msg.drive.speed = 0.5
+        # msg.drive.speed = 0.0
         msg.drive.speed = velocity
-        msg.drive.steering_angle = self.steering_angle
+        msg.drive.steering_angle = angle
         self.drive_pub.publish(msg)
 
 
